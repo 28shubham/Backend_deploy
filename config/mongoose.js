0ee5069fa5ebdf9effa00398730fdd5dca545880
SHUBHAM THAWAIT <sthawait1@gmail.com>
2024-10-09 00:29:36 +0530

    resolve mongodb connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
+// Set strictQuery based on your preference
+mongoose.set("strictQuery", true); // or false, depending on your choice
+
+mongoose
+  .connect("mongodb://localhost:27017/yourdbname", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 mongoose.connect(
   "mongodb+srv://sthawait1:TyepLYhPG62tSU4i@cluster0.wwlsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
