d1196a722ba0e6e6edcf2be599a8c927f44b16a1
SHUBHAM THAWAIT <sthawait1@gmail.com>
2024-10-09 01:27:59 +0530

    correct code

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 //Requiring Modules
-const express = require("express");
+const express = require('express') ;
+const app = express() ;
+const port = 8000 ; 
+const cors = require('cors') ;
+const db = require('./config/mongoose') ;
+const passport = require('passport') ;
+const passport_jwt = require('./config/passport-jwt') ; ;
 
-const cors = require("cors");
-const db = require("./config/mongoose");
-const passport = require("passport");
-const passport_jwt = require("./config/passport-jwt");
 
-const app = express();
-const port = process.env.PORT || 8000;
 //Using Middleware
-app.use(cors());
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-    limit: "50mb",
-  })
-);
-app.get("/", (req, res) => {
-  res.send("Hello, World!"); // or any response you want
-});
+app.use(cors()) ;
+app.use(express.json()) ;
+app.use(express.urlencoded({
+    extended : false  , 
+    limit: '50mb'
+})) ;
 app.use(passport.initialize());
 
+
+
 //diverging request to routes
-const routes = require("./routes");
-app.use("/", routes);
+const routes = require('./routes') ;
+app.use('/' , routes) ;
+
+
 
 //Listning to server
-app.listen(port, (err) => {
-  if (err) {
-    console.log("err");
-  }
-  console.log("Express Server Running on port ", port);
-});
+app.listen(port , (err) => {
+    if(err){
+        console.log("err") ;
+    }
+    console.log("Express Server Running on port " , port) ;
+})
